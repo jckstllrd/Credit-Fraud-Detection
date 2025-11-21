3b38baeeeb3be7ce2be2f722dbf6293c81d01af6
jckstllrd <jackstallard17@gmail.com>
2025-11-21 21:52:53 +0000

    Fix issues with module installs and add basic info_summary function for displaying csv information

--- src/fraud_detection/data_loader.py
+++ src/fraud_detection/data_loader.py
@@ -2,5 +2,12 @@ import pandas as pd
 
 def load_data(csv):
     df = pd.read_csv(csv)
-    print(csv)
     return df
+
+def info_summary(df):
+    shape = df.shape
+    num_duplicates = df.duplicated().sum()
+    
+    summary = f"Data Shape: {shape}\nDuplicate Rows: {num_duplicates}"
+    
+    return summary
